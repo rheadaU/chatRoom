6f6a033e6c427afa3f3157f8643802d254b99ca5
jlouie2 <jlouie2@my.hpu.edu>
2017-05-04 10:19:44 -1000

    X frame button
    X frame button can disconnect client too

--- ChatroomClient/chatClientGUI.java
+++ ChatroomClient/chatClientGUI.java
@@ -1,30 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pkg3401project;
 
 /**
  *
  * @author Jocelyn
  */
+import java.awt.event.WindowEvent;
+import java.io.FileOutputStream;
 import javax.swing.*;
 import java.io.PrintWriter;
 import java.net.*;
+import java.sql.Timestamp;
 import java.util.Scanner;
 
 public class chatClientGUI extends JFrame {
     //Globals
 
-    private static chatClient ChatClient;
-    public static String UserName = "Annoymous";
+    private static ChatClient ChatClient;
+    public static String UserName = "Anonymous";
+
+    public static String UserList = "";
+    //for log
+    public static PrintWriter writer;//log
+    public static final int TIMEOUT = 10000;
 
     public static JFrame MainWindow = new JFrame();
-    private static JButton B_About = new JButton("About");
     private static JButton B_Connect = new JButton("Connect");
-    private static JButton B_Disconnect = new JButton("Disconnect");
-    private static JButton B_Help = new JButton("Help");
+    private static JButton B_Disconnect = new JButton("Bye");
     private static JButton B_Send = new JButton("Send");
     private static JLabel L_Message = new JLabel("Message: ");
     public static JTextField TF_Message = new JTextField(20);
@@ -37,7 +38,7 @@ public class chatClientGUI extends JFrame {
     private static JLabel L_LoggedInAs = new JLabel("Logged in as");
     private static JLabel L_LoggedInAsBox = new JLabel();
 
-    //login window
+    //login window    
     public static JFrame LogInWindow = new JFrame();
     public static JTextField TF_UserNameBox = new JTextField(20);
     private static JButton B_Enter = new JButton("Enter");
@@ -48,65 +49,69 @@ public class chatClientGUI extends JFrame {
         Scanner input = new Scanner(System.in);
         System.out.print("Type in a 1 to chat from GUI or 2 to enter command line chat: ");
         String choice = input.next();
-        
+
         //GUI Option
         if (choice.equals("1")) {
             BuildMainWindow();
             Initialize();
-        }
-        //IDE option
+        } //IDE option
         else if (choice.equals("2")) {
             //using command line to chat
-            System.out.println("Please set username now: ");
-            UserName = input.nextLine();
-            Connect();
-            controlLoop();
-            /*
-            * Need to figure out how to know when to recieve message
-            */
 
-        }
-        else{
+            System.out.print("Please set username now: ");
+            UserName = input.next();
+            input.nextLine();
+
+            Connect();
+            chatLoop();
+
+        } else {
             System.out.println("You can't chat with us :p");
         }
-        
 
     }
-    
-    public static void controlLoop() {
+
+    public static void chatLoop() {
         //need to figure out how to know when to recieve a message from the server
         Scanner input = new Scanner(System.in);
         System.out.println("type \"help\" for commands");
         while (true) {
-            //System.out.print("-> ");
-            String cmmd = input.next();
-            if (cmmd.equals("quit")) {
-                break;
-            } else if (cmmd.equalsIgnoreCase("help")) {
-                help();
-            } else if (cmmd.equalsIgnoreCase("send")) {
-                ChatClient.Send(input.next());
-            } 
-              else {
-                System.out.println("unrecognized command: " + cmmd);
-                if (input.hasNext()) {
-                    input.nextLine();
+            try {
+                String s = input.next();
+                if (s.equalsIgnoreCase("bye")) {
+                    System.out.println("Logging out...");
+                    ChatClient.Disconnect("Bye");
+                    break;
+                } else if (s.equalsIgnoreCase("help")) {
+                    help();
+                } else if (s.equalsIgnoreCase("send")) {
+                    ChatClient.Send(input.nextLine().substring(1));
+                } else if (s.equalsIgnoreCase("o_list")) {
+                    System.out.println(UserList);
+                } else {
+                    System.out.println("unrecognized command: " + s);
+                    if (input.hasNext()) {
+                        input.nextLine();
+                    }
                 }
-            }
-            try{
-                ChatClient.Recieve();
-                
-            }catch(IndexOutOfBoundsException e){
-                
+                try {
+                    ChatClient.Recieve();
+
+                } catch (IndexOutOfBoundsException e) {
+
+                }
+            } catch (Exception e) {
+                System.exit(0);
             }
         }
         System.out.println("bye");
     }
-    
+
     private static void help() {
-        System.out.println("send message \t sends message to server");
+        System.out.println("send \t\t sends message to server");
+        System.out.println("o_list \t\t displays who is online");
         System.out.println("help \t\t displays this message");
-        System.out.println("quit \t\t terminate console");
+        System.out.println("bye \t\t leave chat");
     }
 
     public static void Connect() {
@@ -114,13 +119,22 @@ public class chatClientGUI extends JFrame {
             final int port = 8080;
             final String host = "127.0.0.1";
             Socket socket = new Socket(host, port);
+
             System.out.println("You connected to :" + host);
-            ChatClient = new chatClient(socket);
+            ChatClient = new ChatClient(socket);
 
             //send name to add to "Online" list
+            //System.out.println("UserName:" + UserName);
             PrintWriter out = new PrintWriter(socket.getOutputStream());
             out.println(UserName);
             out.flush();
+
+            //start of log file
+            writer = new PrintWriter(new FileOutputStream("./logs/" + UserName + "_log.txt", true));
+            writer.printf("%-23s", new Timestamp(System.currentTimeMillis()));
+            //writer.printf("%1$23s", new Timestamp(date.getTime()));
+            writer.println(":sys: Connected to chat");
+            writer.flush();
 
             Thread thread = new Thread(ChatClient);
             thread.start();
@@ -172,35 +186,33 @@ public class chatClientGUI extends JFrame {
 
         B_Send.setBackground(new java.awt.Color(0, 0, 255));
         B_Send.setForeground(new java.awt.Color(255, 255, 255));
+        B_Send.setOpaque(true);
+        B_Send.setBorderPainted(false);
         MainWindow.getContentPane().add(B_Send);
         B_Send.setBounds(250, 40, 81, 25);
 
         B_Disconnect.setBackground(new java.awt.Color(0, 0, 255));
         B_Disconnect.setForeground(new java.awt.Color(255, 255, 255));
+        B_Disconnect.setOpaque(true);
+        B_Disconnect.setBorderPainted(false);
         MainWindow.getContentPane().add(B_Disconnect);
         B_Disconnect.setBounds(10, 40, 110, 25);
 
         B_Connect.setBackground(new java.awt.Color(0, 0, 255));
         B_Connect.setForeground(new java.awt.Color(255, 255, 255));
+        B_Connect.setOpaque(true);
+        B_Connect.setBorderPainted(false);
         B_Connect.setToolTipText("Connect to server");
         MainWindow.getContentPane().add(B_Connect);
         B_Connect.setBounds(130, 40, 110, 25);
 
-        B_Help.setBackground(new java.awt.Color(0, 0, 255));
-        B_Help.setForeground(new java.awt.Color(255, 255, 255));
-        MainWindow.getContentPane().add(B_Help);
-        B_Help.setBounds(420, 40, 81, 25);
-
-        B_About.setBackground(new java.awt.Color(0, 0, 255));
-        B_About.setForeground(new java.awt.Color(255, 255, 255));
-        MainWindow.getContentPane().add(B_About);
-        B_About.setBounds(340, 40, 75, 25);
-
+        
         MainWindow.getContentPane().add(L_Message);
         L_Message.setBounds(10, 10, 60, 20);
 
         TF_Message.setForeground(new java.awt.Color(0, 0, 255));
         TF_Message.requestFocus();
+        TF_Message.setEditable(false);
         MainWindow.getContentPane().add(TF_Message);
         TF_Message.setBounds(70, 4, 260, 30);
 
@@ -225,7 +237,7 @@ public class chatClientGUI extends JFrame {
         MainWindow.getContentPane().add(L_Online);
         L_Online.setBounds(350, 70, 130, 16);
 
-        String[] TestNames = {"bob", "sue", "jack", "mary"};
+        String[] TestNames = {"Please Connect"};
         JL_Online.setForeground(new java.awt.Color(0, 0, 255));
         JL_Online.setListData(TestNames);
 
@@ -264,6 +276,7 @@ public class chatClientGUI extends JFrame {
             MainWindow.setTitle(UserName + "'s chat box");
             LogInWindow.setVisible(false);
             B_Send.setEnabled(true);
+            TF_Message.setEditable(true);
             B_Disconnect.setEnabled(true);
             B_Connect.setEnabled(false);
             Connect();
@@ -295,21 +308,20 @@ public class chatClientGUI extends JFrame {
             }
         }
         );
-        B_Help.addActionListener(
-                new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                Action_B_Help();
-            }
-        }
-        );
 
-        B_About.addActionListener(
-                new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                Action_B_Help();
+        //When X on frame is pressed, program will disconnect client & stop running
+        MainWindow.addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                if (JOptionPane.showConfirmDialog(MainWindow,
+                        "Are you sure you want to leave the chat?", "Exiting Chat Room",
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
+                    Action_B_Disconnect();
+                }
             }
-        }
-        );
+        });
+
     }//end
 
     public static void Action_B_Send() {
@@ -323,16 +335,10 @@ public class chatClientGUI extends JFrame {
 
     public static void Action_B_Disconnect() {
         try {
-            ChatClient.Disconnect();
+            ChatClient.Disconnect("gui");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    public static void Action_B_Help() {
-        JOptionPane.showMessageDialog(null, "BLah");
-    }
-
-    
 
 }
