57a0a4dcbd62947e8efd46106d1b67b0c30abf93
jlouie2 <jlouie2@my.hpu.edu>
2017-05-04 10:15:01 -1000

    Update chatClient.java

--- ChatroomClient/chatClient.java
+++ ChatroomClient/chatClient.java
@@ -1,25 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pkg3401project;
 
 import java.net.*;
 import java.io.*;
+import java.sql.Timestamp;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
-public class chatClient implements Runnable{
+public class ChatClient implements Runnable{
 	Socket socket;
 	Scanner input;
 	Scanner send = new Scanner(System.in);
 	PrintWriter out;
 	
-	public chatClient(Socket s){
+	public ChatClient(Socket s){
 		this.socket = s;
 	}
 	
+        @Override
 	public void run(){
 		try{
 			try{
@@ -29,7 +26,9 @@ public class chatClient implements Runnable{
 				CheckStream();
 			}
 			finally{
+                            if(!socket.isClosed()){
 				socket.close();
+                            }
 			}
 		}
 		catch(Exception e){
@@ -38,11 +37,22 @@ public class chatClient implements Runnable{
 	
 }//end run
 	
-	public void Disconnect() throws IOException{
+	public void Disconnect(String display) throws IOException{
 		out.println(chatClientGUI.UserName + " has disconnected.");
 		out.flush();
+
+                //log client disconnect
+                chatClientGUI.writer.printf("%-23s", new Timestamp(System.currentTimeMillis()));
+                chatClientGUI.writer.println(":sys: Disconnected from chat");
+                chatClientGUI.writer.flush();
+          
 		socket.close();
-		JOptionPane.showMessageDialog(null, "You disconnected!");
+                
+		if(display.equals("gui")){ 
+                    JOptionPane.showMessageDialog(null, "You disconnected!");
+                }
+       
+                System.out.println("You disconnected!");
 		System.exit(0);
 		
 	}
@@ -66,10 +76,23 @@ public class chatClient implements Runnable{
 				
 				String[] CurrentUsers = temp.split(", ");
 				chatClientGUI.JL_Online.setListData(CurrentUsers);
-			}
-			else{
-				chatClientGUI.TA_Conversation.append(msg + "\n");
-                                if(!msg.contains(chatClientGUI.UserName))System.out.println(msg);
+                                chatClientGUI.UserList = temp;
+			}else if(msg.equals("#syscheck#")){
+                            //notify mother server that we're still here
+                            out.println("#cltrsp#");
+                            out.flush();
+                        }else{
+				chatClientGUI.TA_Conversation.append(msg + "\n"); //GUI
+                                
+                                if(!msg.contains(chatClientGUI.UserName)){
+                                    System.out.println(msg); //cmmnd line
+                                    
+                                    //log client message stream
+                                    chatClientGUI.writer.printf("%-23s", new Timestamp(System.currentTimeMillis()));
+                                    chatClientGUI.writer.println(":rcv: " + msg);
+                                    chatClientGUI.writer.flush();
+                                }
+                                    
 			}
 		}
 	}
@@ -77,8 +100,13 @@ public class chatClient implements Runnable{
 	public void Send(String s){
 		out.println(chatClientGUI.UserName + ": " + s);
 		out.flush();
+                
+                //log client send message log
+                chatClientGUI.writer.printf("%-23s", new Timestamp(System.currentTimeMillis()));
+                chatClientGUI.writer.println(":out: " + s);
+                chatClientGUI.writer.flush();
+                
 		chatClientGUI.TF_Message.setText("");
 	}
 
 }
-
